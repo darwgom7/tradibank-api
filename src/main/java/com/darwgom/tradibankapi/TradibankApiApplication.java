954dfbe317f897439f7d5cf73fa5b5726d0e621e
darwgom7 <darwgom7@gmail.com>
2024-03-09 20:39:35 -0500

    Initial commit

---
+++ src/main/java/com/darwgom/tradibankapi/TradibankApiApplication.java
@@ -0,0 +1,13 @@
+package com.darwgom.tradibankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradibankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradibankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darwgom/tradibankapi/TradibankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwgom.tradibankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradibankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
